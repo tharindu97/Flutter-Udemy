8ffe86ab927573a062b9ceb451fc495eeb2d1c81
Tharindu Kavishna <kavishna@incubatelabs.com>
2021-09-03 08:26:17 +0530

    custom onboarding

---
+++ customonboadingscreen/lib/onboard_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnboardModel {
+  final String img;
+  final String text;
+  final String desc;
+  final Color bg;
+  final Color button;
+
+  OnboardModel({
+    required this.img,
+    required this.text,
+    required this.desc,
+    required this.bg,
+    required this.button,
+  });
+}
